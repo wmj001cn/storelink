656bf6310a5d4ad2486954e4fbc1b9788519d951
wmj001cn <38382180+wmj001cn@users.noreply.github.com>
2018-04-15 01:20:48 +0800

    Add files via upload

---
+++ fwsk.js
@@ -0,0 +1,466 @@
+//默认菜
+var defaultk = [
+	
+	{
+        'url': '6ed5wecb3', 
+        'desc': '珠江纯生啤酒', 
+        'sku': 'A002', 
+        'price': 10, 
+        'qty': 0
+ },
+ 
+ {
+     'url': 'vx5i9egfj', 
+     'desc': '哈尔滨啤酒', 
+     'sku': 'A003', 
+     'price': 10, 
+     'qty': 0
+ },
+ {
+     'url': 'cfautgr7j', 
+     'desc': '嘉士伯啤酒', 
+     'sku': 'A004', 
+     'price': 10, 
+     'qty': 0
+},
+
+{
+    'url': 'wzforxrj3', 
+    'desc': '百威啤酒', 
+    'sku': 'A007', 
+    'price': 10, 
+    'qty': 0
+},
+]
+
+//招牌菜
+var gd =[
+    {
+        'url': '640lg349r', 
+        'desc': '烧烤炒田螺', 
+        'sku': 'A001', 
+        'price': 15, 
+        'qty': 0
+ },
+ {
+        'url': '4c7ml6d73', 
+        'desc': '大口螺', 
+        'sku': 'A002', 
+        'price': 25, 
+        'qty': 0
+ },
+ {
+        'url': 's3703astr', 
+        'desc': '烧烤飞机排骨', 
+        'sku': 'A003', 
+        'price': 35, 
+        'qty': 0
+ },
+{
+        'url': 'ad5bi9uof', 
+        'desc': '烧烤生蚝', 
+        'sku': 'A004', 
+        'price':60, 
+        'qty': 0
+ },
+{
+        'url': 'vzkbzb0yn', 
+        'desc': '烧烤鱼', 
+        'sku': 'A005', 
+        'price': 80, 
+        'qty': 0
+ },
+]
+
+//水产
+var marine =  [
+    
+
+    {
+           'url': '4c7ml6d73', 
+           'desc': '大口螺', 
+           'sku': 'A034', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': '640lg349r', 
+           'desc': '炒田螺', 
+           'sku': 'A035', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'ad5bi9uof', 
+           'desc': '烤生蚝', 
+           'sku': 'A036', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'vzkbzb0yn', 
+           'desc': '烤全鱼', 
+           'sku': 'A037', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': '52rmidrvz', 
+           'desc': '烤扇贝', 
+           'sku': 'A038', 
+           'price': 10, 
+           'qty': 0
+    },
+
+    {
+           'url': '5fj0okplb', 
+           'desc': '烤青口', 
+           'sku': 'A039', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'w0ljk44tb', 
+           'desc': '烤龙虱', 
+           'sku': 'A040', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'z7g33r4ov', 
+           'desc': '烤多春鱼', 
+           'sku': 'A041', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': '89m621ucf', 
+           'desc': '烤多春鱼', 
+           'sku': 'A042', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': '9onqqrnpr', 
+           'desc': '烤大鱿鱼', 
+           'sku': 'A043', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'vnu5dywu7', 
+           'desc': '烤干鱿鱼', 
+           'sku': 'A044', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'bt83ru273', 
+           'desc': '烤白仓鱼', 
+           'sku': 'A045', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'f02nbi4nj', 
+           'desc': '烤秋刀鱼', 
+           'sku': 'A046', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'r1y15ntbj', 
+           'desc': '烤鱼丸串', 
+           'sku': 'A047', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': '8z4yefpr3', 
+           'desc': '烤鱼板', 
+           'sku': 'A048', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'sgzlud79b', 
+           'desc': '烤鱿鱼', 
+           'sku': 'A049', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'str00jhtb', 
+           'desc': '烤黄鱼', 
+           'sku': 'A050', 
+           'price': 10, 
+           'qty': 0
+    },
+            
+];
+
+//肉类
+var meat = [
+    
+
+    {
+           'url': 's3703astr', 
+           'desc': '飞机排', 
+           'sku': 'A008', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'r1y15syi7', 
+           'desc': '烤火腿肠仔', 
+           'sku': 'A009', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'w0ljkdkbj', 
+           'desc': '烤肉串', 
+           'sku': 'A010', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'y55wlgtnz', 
+           'desc': '烤肉丸串', 
+           'sku': 'A011', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'tj9sd35jz', 
+           'desc': '烤鸡中翅', 
+           'sku': 'A012', 
+           'price': 10, 
+           'qty': 0
+    },
+
+    {
+           'url': 'x2vq2vsu7', 
+           'desc': '烤全鸡翅', 
+           'sku': 'A013', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'cviaalxxr', 
+           'desc': '烤鸡翅尖', 
+           'sku': 'A014', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': '5saeuzksf', 
+           'desc': '烤鸡肾串', 
+           'sku': 'A015', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'qp6mzmvnz', 
+           'desc': '烤鸡脚串', 
+           'sku': 'A016', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'pznunaajz', 
+           'desc': '烤鸡全脚', 
+           'sku': 'A017', 
+           'price': 10, 
+           'qty': 0
+    },
+            
+];
+
+//酒水
+var liquid = [
+    
+
+    {
+           'url': 'cfautfomn/1664.jpg', 
+           'desc': '1664啤酒', 
+           'sku': 'A001', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': '6ed5wecb3', 
+           'desc': '珠江纯生啤酒', 
+           'sku': 'A002', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'vx5i9egfj', 
+           'desc': '哈尔滨啤酒', 
+           'sku': 'A003', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'cfautgr7j', 
+           'desc': '嘉士伯啤酒', 
+           'sku': 'A004', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 't32cvxytr', 
+           'desc': '生力清啤酒（旧款）', 
+           'sku': 'A005', 
+           'price': 10, 
+           'qty': 0
+    },
+
+    {
+           'url': 'qlqlophi7', 
+           'desc': '生力清啤酒', 
+           'sku': 'A006', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'wzforxrj3', 
+           'desc': '百威啤酒', 
+           'sku': 'A007', 
+           'price': 10, 
+           'qty': 0
+    },
+            
+];
+
+//蔬菜
+var vegi = [
+    
+
+    {
+           'url': 'mxx2dkacf', 
+           'desc': '烤冬菇', 
+           'sku': 'A018', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': '81yj5zedb', 
+           'desc': '烤水瓜', 
+           'sku': 'A019', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'h9qrmpvq7', 
+           'desc': '烤玉米', 
+           'sku': 'A020', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'xkqviy0hb', 
+           'desc': '烤白菇', 
+           'sku': 'A021', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'l643ioe4v', 
+           'desc': '烤秋葵', 
+           'sku': 'A022', 
+           'price': 10, 
+           'qty': 0
+    },
+
+    {
+           'url': 'e2w832gf3', 
+           'desc': '烤粟子', 
+           'sku': 'A023', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'ml5o7cphr', 
+           'desc': '烤鸡翅尖', 
+           'sku': 'A024', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': '4iclg592n', 
+           'desc': '烤芥兰', 
+           'sku': 'A025', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'm8ea18k3j', 
+           'desc': '烤茄瓜', 
+           'sku': 'A026', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'blkgvqrcv', 
+           'desc': '烤娃娃菜', 
+           'sku': 'A027', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'jeb4nqs73', 
+           'desc': '烤豆腐串', 
+           'sku': 'A028', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': '5xe64vplb', 
+           'desc': '烤豆角', 
+           'sku': 'A029', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'ktcpcgdun', 
+           'desc': '烤辣椒', 
+           'sku': 'A030', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'dddfqpnlb', 
+           'desc': '烤金针菇', 
+           'sku': 'A031', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'blkgvqrcv', 
+           'desc': '烤韭菜', 
+           'sku': 'A032', 
+           'price': 10, 
+           'qty': 0
+    },
+    {
+           'url': 'lvmvv29jj', 
+           'desc': '烤韭菜', 
+           'sku': 'A033', 
+           'price': 10, 
+           'qty': 0
+    },
+            
+];
+
+//以下忽略
+var defaultk = gd.concat(defaultk);
+var myall = {
+	'default' : defaultk,
+	'gd' : gd,
+	'marine' : marine,
+	'meat' : meat,
+	'vegi' : vegi,
+	'liquid' : liquid
+}
